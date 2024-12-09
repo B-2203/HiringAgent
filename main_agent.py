a026d00773733ec60067d366dc050c2c61257d2f
Mahesh <maheshp@e42.ai>
2024-12-09 15:52:00 +0530

    skills quetion changes

--- main_agent.py
+++ main_agent.py
@@ -1,3 +1,4 @@
+import pdb
 from langchain_core.messages import SystemMessage
 from langgraph.graph import StateGraph, START, END
 from langchain_core.messages import AIMessage, HumanMessage, ToolMessage
@@ -6,17 +7,47 @@ from langchain_core.runnables import RunnableConfig
 from uuid import uuid4
 
 
-from states import greeting, info, InformationGatheringState, InitialState
+from states import certifications_collect, experience_collect, greeting, info, skills_collect, skills_question, InformationGatheringState, InitialState
 
+def router(state:InformationGatheringState):
+    pdb.set_trace()
+    if "name" not in state or not state["name"]:
+        return "greeting"
+    if "skills" not in state or not state["skills"]:
+        return "skills_collect"
+    if "experience" not in state or not state["experience"]:
+        return "experience_collect"
+    if "certifications" not in state or not state["certifications"]:
+        return "certifications_collect"
+    if  state.get('name', '') and  state.get('skills', '') and  state.get('experience', '') and state.get('certifications', '') and state['counter'] < 4:
+        return "skills_question"
+    if state['counter'] >= 4:
+        "end"
+    else:
+        return "info"
     
 
-workflow = StateGraph(InformationGatheringState, input=InitialState, output=InformationGatheringState)
+workflow = StateGraph(InitialState)
 
 workflow.add_node("greeting", greeting)
+workflow.add_node("skills_collect", skills_collect)
+workflow.add_node("experience_collect", experience_collect)
+workflow.add_node("certifications_collect", certifications_collect)
 workflow.add_node("info", info)
-workflow.add_edge(START, "greeting")
-workflow.add_edge("greeting", "info")
-workflow.add_edge("info", END)
+workflow.add_node("skills_question", skills_question)
+workflow.add_edge(START, "info")
+# workflow.add_edge("greeting", "info")
+# workflow.add_edge("skills_collect", "experience_collect")
+# workflow.add_edge("experience_collect", "certifications_collect")
+# workflow.add_edge("certifications_collect", "info")
+
+workflow.add_conditional_edges(
+    "info",
+    router,
+    {"greeting": "greeting", "info": "info", "skills_collect" : 'skills_collect',"experience_collect":"experience_collect", "skills_question": "skills_question","certifications_collect":"certifications_collect", "end": END}
+)
+# workflow.add_edge("skills_collect", END)
+
 
 
 memory = MemorySaver()
@@ -31,9 +62,9 @@ def stream_graph_updates(user_input: str):
     input_message = HumanMessage(content=user_input) 
     for event in graph.stream({"messages": [input_message]}, config=config, stream_mode="updates"):
         for value in event.values():
-            # print("Assistant:", value["messages"][-1].pretty_print())
-            if value:
-                print("Assistant:", value["messages"].content)
-                return value["messages"].content
+            # print("Assistant:", value["messages"])
+            if value and "messages" in value:
+                print("Assistant:", value["messages"])
+                return value["messages"][-1].content
     else:
         return "Sorry, I don't have a response at this moment!" 
--- states.py
+++ states.py
@@ -2,74 +2,196 @@ from typing import List, TypedDict, Literal, Annotated, Optional
 from langchain_core.messages import AIMessage, HumanMessage, ToolMessage, SystemMessage
 from langgraph.graph.message import add_messages
 from langchain_ollama import ChatOllama
+from langchain_core.prompts import PromptTemplate
 from pydantic import BaseModel, Field
+import pdb
 
 
-info_gathering_prompt = """Your are a technical interviewer, so introduce yourselves as a Technical recruiter from E42.ai before you go about the remaining tasks, your job is to ask questions to the user and collect the entities : ['name', 'skills', 'experience', 'certifications'].
-- STRICTLY give json output containing the above entities, but do not mention this to the user. 
+info_gathering_name_prompt = """Your are a interviewer, so introduce yourselves as a recruiter from E42.ai , your job is to ask questions to the user for getting users name.
+Design a user-friendly question that effectively prompts users to provide their name.The question should be clear, concise, and inviting, ensuring that users feel comfortable responding.Consider various contexts in which this question might be asked, such as in a registration form, a chatbot interaction, or a survey.Additionally, provide variations of the question to accommodate different tones (formal, casual, friendly).
+
 - STRICTLY ask the full name of the user when you start.
-- STRICTLY get only the following information from them:
+- STRICTLY ONLY ask name.
+- STRICTLY DO NOT ask technical interview questions.
 
-- What is the user's name
-- What skills the user possess
-- How many years of experience the user has
-- Any certifications done by the user
+"""
+info_gathering_skills_prompt = """Your are a technical interviewer, so introduce yourselves as a Technical recruiter from E42.ai, your job is to ask questions to the user for the entities : ['skills'].
+  - STRICTLY dont introduce youreself.
+  - STRICTLY dont add skills by youreself get it from user only.
+
+"""
+info_gathering_experience_prompt = """Your are a technical interviewer,  your job is to ask questions to the user for the entities : ['experience'].
+  - STRICTLY dont introduce youreself.
+
+"""
+info_gathering_certifications_prompt = """Your are a technical interviewer, so introduce yourselves as a Technical recruiter from E42.ai before you go about the remaining tasks, your job is to ask questions to the user for the entities : ['certifications'].
+   - STRICTLY dont introduce youreself. 
+
 """
 
-gather_entities = """Extract entites from the text if any of them not there fill them with --NA--.
+gather_entities = """Extract entites from the text if any of them.
 Don't introduce yourselves, directly go about extracting entities. 
-STRICTLY give json output containing below entities, but do not mention this to the user. 
+STRICTLY give json output containing below entities 
+
 
 The entities you need to extract are:
-- 'name': The name of the user.
-- 'skills': The skill sets of a user.
-- 'experience': The years of experience the user possess.
-- 'certifications': Certifications user has completed, keep it blank if the user says no or does not mention.
+- `name`: The name of the user.
+- `skills`: The skill sets of a user.
+- `experience`: The years of experience the user possess.
+- `certifications`: Certifications user has completed, keep it blank if the user says no or does not mention.
+
+Note
+- If skills is not available,  keep it `['NA']` .
+- If certifications is not available,  keep it `['NA'].
+
 """
+p = """
+You are an Intelligent Extractor Assistant You will be provided with a message containing an individual’s professional profile. Your task is to extract the following entites:
+
+Name: The name of the individual.
+Skills: A list of skills mentioned.
+Experience: duration work experience.
+Certificates: Any certifications or qualifications mentioned.
+
+Note
+- STRICTLY give structured output
+- If name is not available,  keep it '' .
+
+
+
+"""
+
+ask_queries = """You’re a seasoned technical interviewer with over 15 years of experience in assessing candidates for various roles in technology. You specialize in crafting insightful and challenging interview questions that effectively gauge both technical skills and problem-solving abilities, tailored to each candidate's expertise.
+
+Your task is to generate a technical interview QUESTION based on the skills provided. Here are the details you need to consider:
+
+- Skills to Assess: {skills}
+
+Please note:
+- STRICTLY Don't add any additional text other than question.
+
+Please ensure that the questions cover a range of difficulty levels and include both theoretical and practical components, while also being relevant to current industry standards and technologies."""
 
 class InitialState(TypedDict):
     messages: Annotated[list, add_messages]
+    counter: int
+
 
 class InformationGatheringState(TypedDict):
-    name: str
-    skills: list
-    experience: str
-    certifications: list
+    name: str = ''
+    skills: list = []
+    experience: str = []
+    certifications: list =[]
     messages: Annotated[list, add_messages]
     ai_message: Annotated[list, add_messages] 
+    counter: int
 
 class InformationGathered(BaseModel):
     """Information gathered from candidate"""
     name: Optional[str] = Field(..., description='Name of the user')
-    skills: Optional[List[str]] = Field(..., description='Skills listed by the user')
+    skills: Optional[str] = Field(..., description='Skills listed by the user')
     experience: Optional[str] = Field(..., description='Years of experience a user has')
-    certifications: Optional[List[str]] = Field(..., description='Certifications listed by the user')
+    certifications: Optional[str] = Field(..., description='Certifications listed by the user')
 
 llm = ChatOllama(model = "llama3.2", temperature=0)
 llm_structured = llm.with_structured_output(InformationGathered)
 
-
-def greeting(state: InitialState) -> InformationGatheringState:
+def greeting(state: InitialState) -> InitialState:
     
-    response = llm.invoke([SystemMessage(content=info_gathering_prompt)] +
+    response = llm.invoke([SystemMessage(content=info_gathering_name_prompt)] +
                                 state['messages'])
-    return {'messages': response}
+    
+    state["counter"] =  0
+   
+    state['messages'] = state['messages'] + [response]
+    return state
+    
+
+def skills_collect(state: InformationGatheringState) -> InformationGatheringState:
+    response = llm.invoke([SystemMessage(content=info_gathering_skills_prompt)] +
+                                state['messages'])
+    
+    # state["counter"] +=  1
+    data = {}
+    state['messages'] = state['messages'] + [response]
+    return state
+
+def experience_collect(state: InformationGatheringState) -> InformationGatheringState:
+    response = llm.invoke([SystemMessage(content=info_gathering_experience_prompt)] +
+                                state['messages'])
+    
+    state['messages'] = state['messages'] + [response]
+    return state
+
+def certifications_collect(state: InformationGatheringState) -> InformationGatheringState:
+    response = llm.invoke([SystemMessage(content=info_gathering_certifications_prompt)] +
+                                state['messages'])
+    
+    # state["counter"] +=  1
+    data = {}
+    
+    state['messages'] = state['messages'] + [response]
+    pdb.set_trace()
+    return state
+    # response = llm_structured.invoke([SystemMessage(content=info_gathering_prompt)] +
+    #                             messages)
+    # if response:
+       
+    #     data = {}
+    #     for entity_name,  entity_value in response:
+    #         if entity_name not in state.keys() and entity_value != '--NA--':
+    #             data[entity_name] = entity_value
+
+    #     data.setdefault("messages",response)
+    #     state.update(data)
+    # return state
 
 
 def info(state: InformationGatheringState):
+    
     messages = state['messages']
     # state['conversations'].append(messages)
     # state['conversations'].append(llm.invoke([SystemMessage(content=info_gathering_prompt)] +
     #                             state["conversations"]))
-    response = llm_structured.invoke([SystemMessage(content=gather_entities),
-                                messages[-2]])
-    if response:
-        data = {}
-        for entity_name,  entity_value in response:
-            if entity_name not in state.keys() and entity_value != '--NA--':
-                data[entity_name] = entity_value
+    
+    respone = llm_structured.invoke([SystemMessage(content=p)]+
+                                messages)
+    
+    
+       
+    # if op_:
+    #     output_dict = {}
+    #     op_dict = op_.dict()
+    #     
+    #     # keys should be dynamic from Pydantic models
+    #     for key in ['name', 'skills', 'experience', 'certifications']:
+    #         if not state.get(key, '') and op_dict.get(key):
+    #             output_dict[key] = op_dict[key]
+    #     if output_dict:
+    #         # print('\t DEBUG: Output ', output_dict)
+    #         output_dict['counter'] = 
+    #         return output_dict
+    return respone
 
-        data.setdefault("messages", messages[-1])
-        state.update(data)
+def skills_question(state: InformationGatheringState):
+    messages = state['messages']
+    # state['conversations'].append(messages)
+    # state['conversations'].append(llm.invoke([SystemMessage(content=info_gathering_prompt)] +
+    #                             state["conversations"])
+    print("innn msg skills")
+    state["counter"] +=  1
+    print(messages)
+    prompt = PromptTemplate(
+        input_variables=["skills"],
+        template=ask_queries,
+    )
+
+    
+    final_prompt = prompt.format(skills=state["skills"])
+    response = llm.invoke([SystemMessage(content=final_prompt)] +
+                                state['messages'])
+    
+    # state["counter"] +=  1
+    state['messages'] = state['messages'] + [response]
     return state
 
